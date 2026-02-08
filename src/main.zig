048734529d9870bcb3bd496e01e456e593ca26a9
James Cox-Morton <th3james@hey.com>
2026-02-08 09:14:56 +0000

    Add basic paddle movement

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,16 @@ const rl = @cImport(@cInclude("raylib.h"));
 const window_width = 800;
 const window_height = 600;
 
+const Vec2 = struct {
+    x: f32,
+    y: f32,
+};
+
 const paddle_width = 100;
 const paddle_height = 20;
-const paddle_y = 550;
 const paddle_speed = 8;
+
+const ball_size = 20;
 
 pub fn main() void {
     rl.InitWindow(window_width, window_height, "Breakout");
@@ -14,20 +20,39 @@ pub fn main() void {
 
     rl.SetTargetFPS(60);
 
-    var paddle_x: f32 = @as(f32, (window_width - paddle_width)) / 2.0;
+    var paddle_pos = Vec2{
+        .x = @as(f32, (window_width - paddle_width)) / 2.0,
+        .y = 550,
+    };
+
+    var ball_pos = Vec2{
+        .x = paddle_pos.x,
+        .y = paddle_pos.y - 50,
+    };
 
     while (!rl.WindowShouldClose()) {
-        // Update
-        if (rl.IsKeyDown(rl.KEY_LEFT)) paddle_x -= paddle_speed;
-        if (rl.IsKeyDown(rl.KEY_RIGHT)) paddle_x += paddle_speed;
+        // -- Modify world --
+        // Move paddle
+        if (rl.IsKeyDown(rl.KEY_LEFT)) paddle_pos.x -= paddle_speed;
+        if (rl.IsKeyDown(rl.KEY_RIGHT)) paddle_pos.x += paddle_speed;
 
-        if (paddle_x < 0) paddle_x = 0;
-        if (paddle_x > window_width - paddle_width) paddle_x = window_width - paddle_width;
+        // clamp paddle to world space
+        if (paddle_pos.x < 0) paddle_pos.x = 0;
+        if (paddle_pos.x > window_width - paddle_width) paddle_pos.x = window_width - paddle_width;
 
-        // Draw
+        // Move ball
+        ball_pos.y += 1;
+
+        // -- Render --
         rl.BeginDrawing();
         rl.ClearBackground(rl.RAYWHITE);
-        rl.DrawRectangle(@intFromFloat(paddle_x), paddle_y, paddle_width, paddle_height, rl.DARKGRAY);
+
+        // Draw paddle
+        rl.DrawRectangle(@intFromFloat(paddle_pos.x), @intFromFloat(paddle_pos.y), paddle_width, paddle_height, rl.DARKGRAY);
+
+        // Draw Ball
+        rl.DrawRectangle(@intFromFloat(ball_pos.x), @intFromFloat(ball_pos.y), ball_size, ball_size, rl.DARKGRAY);
+
         rl.EndDrawing();
     }
 }
