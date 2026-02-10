6669299068fa84aef7859b5a12b0163b8a0ab388
James Cox-Morton <th3james@hey.com>
2026-02-10 20:47:13 +0000

    Add death

--- src/main.zig
+++ src/main.zig
@@ -5,8 +5,7 @@ const window_width = 800;
 const window_height = 600;
 
 const Rectangle = struct {
-    x: f32,
-    y: f32,
+    pos: Position,
     width: f32,
     height: f32,
 };
@@ -16,6 +15,8 @@ const Velocity = struct {
     y: f32,
 };
 
+const Position = Velocity;
+
 const paddle_width = 100;
 const paddle_height = 20;
 const paddle_speed = 8;
@@ -23,15 +24,15 @@ const paddle_speed = 8;
 const ball_size = 20;
 
 fn bounce(ball: Rectangle, thing: Rectangle) Velocity {
-    const overlap_x = @min(ball.x + ball.width, thing.x + thing.width) - @max(ball.x, thing.x);
-    const overlap_y = @min(ball.y + ball.height, thing.y + thing.height) - @max(ball.y, thing.y);
+    const overlap_x = @min(ball.pos.x + ball.width, thing.pos.x + thing.width) - @max(ball.pos.x, thing.pos.x);
+    const overlap_y = @min(ball.pos.y + ball.height, thing.pos.y + thing.height) - @max(ball.pos.y, thing.pos.y);
 
     if (overlap_x <= 0 or overlap_y <= 0) return .{ .x = 0, .y = 0 };
 
     // Minimum penetration indicates the most recent axis to overlap, and therefore the colliding wall
     if (overlap_x < overlap_y) {
-        const ball_mid = ball.x + (ball.width / 2);
-        const thing_mid = thing.x + (thing.width / 2);
+        const ball_mid = ball.pos.x + (ball.width / 2);
+        const thing_mid = thing.pos.x + (thing.width / 2);
         // Which side collided?
         if (ball_mid < thing_mid) {
             return .{ .x = -overlap_x, .y = 0 };
@@ -39,8 +40,8 @@ fn bounce(ball: Rectangle, thing: Rectangle) Velocity {
             return .{ .x = overlap_x, .y = 0 };
         }
     } else {
-        const ball_mid = ball.y + (ball.height / 2);
-        const thing_mid = thing.y + (thing.height / 2);
+        const ball_mid = ball.pos.y + (ball.height / 2);
+        const thing_mid = thing.pos.y + (thing.height / 2);
         // Which side collided?
         if (ball_mid < thing_mid) {
             return .{ .x = 0, .y = -overlap_y };
@@ -57,70 +58,76 @@ pub fn main() void {
     rl.SetTargetFPS(60);
 
     var paddle = Rectangle{
-        .x = @as(f32, (window_width - paddle_width)) / 2.0,
-        .y = 550,
+        .pos = .{
+            .x = @as(f32, (window_width - paddle_width)) / 2.0,
+            .y = 550,
+        },
         .height = paddle_height,
         .width = paddle_width,
     };
 
-    var ball = Rectangle{
+    const ball_start_pos = Position{
         .x = @as(f32, (window_width - ball_size)) / 2.0,
-        .y = paddle.y - 50,
+        .y = paddle.pos.y - 50,
+    };
+    var ball = Rectangle{
+        .pos = ball_start_pos,
         .width = ball_size,
         .height = ball_size,
     };
-    var ball_velocity = Velocity{ .x = 5, .y = 5 };
+    const start_velocity = Velocity{ .x = 5, .y = 5 };
+    var ball_velocity = start_velocity;
 
     while (!rl.WindowShouldClose()) {
         // -- Modify world --
         // Move paddle
-        if (rl.IsKeyDown(rl.KEY_LEFT)) paddle.x -= paddle_speed;
-        if (rl.IsKeyDown(rl.KEY_RIGHT)) paddle.x += paddle_speed;
+        if (rl.IsKeyDown(rl.KEY_LEFT)) paddle.pos.x -= paddle_speed;
+        if (rl.IsKeyDown(rl.KEY_RIGHT)) paddle.pos.x += paddle_speed;
 
         // clamp paddle to world space
-        if (paddle.x < 0) paddle.x = 0;
-        if (paddle.x > window_width - paddle_width) paddle.x = window_width - paddle_width;
+        if (paddle.pos.x < 0) paddle.pos.x = 0;
+        if (paddle.pos.x > window_width - paddle_width) paddle.pos.x = window_width - paddle_width;
 
         // Move ball
-        ball.x += ball_velocity.x;
-        ball.y += ball_velocity.y;
+        ball.pos.x += ball_velocity.x;
+        ball.pos.y += ball_velocity.y;
 
         // Clamp ball to world space
         if (ball_velocity.x > 0) {
-            const ball_x_edge = ball.x + ball_size;
+            const ball_x_edge = ball.pos.x + ball_size;
             const overshoot = ball_x_edge - window_width;
             if (overshoot > 0) {
                 ball_velocity.x = -ball_velocity.x;
-                ball.x = window_width - overshoot - ball_size;
+                ball.pos.x = window_width - overshoot - ball_size;
             }
         } else {
-            if (ball.x < 0) {
+            if (ball.pos.x < 0) {
                 ball_velocity.x = -ball_velocity.x;
-                ball.x = -ball.x;
+                ball.pos.x = -ball.pos.x;
             }
         }
 
         if (ball_velocity.y > 0) {
-            const ball_y_edge = ball.y + ball_size;
+            const ball_y_edge = ball.pos.y + ball_size;
             const overshoot = ball_y_edge - window_height;
             if (overshoot > 0) {
-                ball_velocity.y = -ball_velocity.y;
-                ball.y = window_height - overshoot - ball_size;
+                ball_velocity = start_velocity;
+                ball.pos = ball_start_pos;
             }
         } else {
-            if (ball.y < 0) {
+            if (ball.pos.y < 0) {
                 ball_velocity.y = -ball_velocity.y;
-                ball.y = -ball.y;
+                ball.pos.y = -ball.pos.y;
             }
         }
 
         // Bounce off paddle
         const rebound = bounce(ball, paddle);
         if (rebound.x != 0) {
-            ball.x += rebound.x;
+            ball.pos.x += rebound.x;
             ball_velocity.x = -ball_velocity.x;
         } else if (rebound.y != 0) {
-            ball.y += rebound.y;
+            ball.pos.y += rebound.y;
             ball_velocity.y = -ball_velocity.y;
         }
 
@@ -129,10 +136,10 @@ pub fn main() void {
         rl.ClearBackground(rl.RAYWHITE);
 
         // Draw paddle
-        rl.DrawRectangle(@intFromFloat(paddle.x), @intFromFloat(paddle.y), paddle_width, paddle_height, rl.DARKGRAY);
+        rl.DrawRectangle(@intFromFloat(paddle.pos.x), @intFromFloat(paddle.pos.y), paddle_width, paddle_height, rl.DARKGRAY);
 
         // Draw Ball
-        rl.DrawRectangle(@intFromFloat(ball.x), @intFromFloat(ball.y), ball_size, ball_size, rl.DARKGRAY);
+        rl.DrawRectangle(@intFromFloat(ball.pos.x), @intFromFloat(ball.pos.y), ball_size, ball_size, rl.DARKGRAY);
 
         rl.DrawFPS(10, 10);
         var buf: [64]u8 = undefined;
